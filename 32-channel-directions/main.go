8acbdc968fd54bd4501ccf14d559d4a09ff903b8
SilverSurge <deepkumarpatel471@gmail.com>
2025-09-20 14:29:02 +0530

    add channel synchronization, channel directions, select

---
+++ 32-channel-directions/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func ping(pings chan<- string, msg string) {
+	pings <- msg
+}
+
+func pong(pings <-chan string, pongs chan<- string) {
+	msg := <-pings
+	pongs <- msg
+}
+
+func main() {
+	fmt.Println("Channel Directions")
+
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+	ping(pings, "ping-ping-ping")
+	pong(pings, pongs)
+	fmt.Println(<-pongs)
+}
